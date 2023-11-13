cda2362b5405bae2c28edc9e3b79b004b2c3d180
ent1k1337 <ent1k1337@yandex.ru>
2023-11-13 19:23:24 +0300

    add env file

--- main.go
+++ main.go
@@ -4,20 +4,19 @@ import (
 	"database/sql"
 	"github.com/ent1k1377/simplebank/api"
 	db "github.com/ent1k1377/simplebank/db/sqlc"
+	"github.com/ent1k1377/simplebank/util"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	var err error
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
@@ -25,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
